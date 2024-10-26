503bdcba381232b87e727c7e855bc764108ff169
Faaiz Abdullah <faaizabdullah@MacBook-Air.hitronhub.home>
2024-10-25 22:22:28 -0700

    Edited about page

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
 
     return (
-        <section className="pt-40">
+        <section className="lg:pt-28 lg:ml-24 lg:text-8xl font-interTight font-bold">
             <h1>About me</h1>
 
 
